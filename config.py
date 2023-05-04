4cf993c942600f9f29ade562f3110be86ac57d15
Krishan-kumar-surela <86333128+Krishan-kumar-surela@users.noreply.github.com>
2023-05-04 22:32:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ OWNER_ID =  int(os.environ.get("OWNER_ID", "6199578420"))
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(6199578420)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "@kpslink_official")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Kpslink_official") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001911018111"))
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Kpslink") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
