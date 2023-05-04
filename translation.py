0dcc0f7c70e97ee4921778c80f46ae427c0d5317
Krishan-kumar-surela <86333128+Krishan-kumar-surela@users.noreply.github.com>
2023-05-04 16:42:20 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,9 +3,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 START_MESSAGE = '''**Hello, {}
-I Am ShortnerFly.com, Bulk Link Converter. I Can Convert Links Directly From Your ShortnerFly Account,
+I am KPSLINK.COM, Bulk Link Converter. I Can Convert Links Directly From Your KPSLINK.COM Account,
     
-1. Go To 👉 https://ShortnerFly.com/member/tools/api  
+1. Go To 👉 https://kpslink.com/member/tools/api  
 2. Than Copy API Key
 3. Than Type /api than give a single space and than paste your API Key (see example to understand more...)**
 
@@ -18,8 +18,8 @@ Example:** `/api de303d5270f481aec928f39883da7b7f9a8812ac `
 **➕ Hit** 👉 /channel Command To Get Help About Adding your channel to bot.
 **➕ Hit** 👉 /footer To Get Help About Adding your Custom Footer to bot.
 
-If You Want Any **Other Shortner** Link Converter Bot Instead Of Shortnerfly than **contact** at 👉 @Cyniteofficial (all **shortners** support available.)
-'''
+
+
 
 HELP_MESSAGE = '''**Hello, {}
 I Am ShortnerFly, Bulk Link Converter Bot. I Can Convert Links Directly From Your ShortnerFly Account,**
@@ -96,7 +96,7 @@ HELP_REPLY_MARKUP = InlineKeyboardMarkup([
 
 START_MESSAGE_REPLY_MARKUP  = InlineKeyboardMarkup([
     [
-        InlineKeyboardButton('Get Api', url=f'https://ShortnerFly.com/member/tools/api')
+        InlineKeyboardButton('Get Api', url=f'https://kpslink.com/member/tools/api')
     ]
 ])
 
@@ -109,12 +109,12 @@ BACK_REPLY_MARKUP = InlineKeyboardMarkup([
 
 ])
 
-USER_ABOUT_MESSAGE = """
-- Website: [{base_site}](https://shortnerfly/ref/rngharman)
 
-- Site Link {base_site} Current Linked API: {shortener_api}
 
-- Replace Channel Username: @{username}
+
+
+
+
 
 - Header Text: 
 {header_text}
@@ -131,7 +131,7 @@ SHORTENER_API_MESSAGE = """To add or update your Shortner Website API,
             
 Ex: `/api de303d5270f481aec928f39883da7b7f9a8812ac `
 
-Get API From [{base_site}](https://shortnerfly/ref/rngharman)
+Get API From [{base_site}](https://kpslink.com/ref/rngharman)
 
 Current {base_site} API: `{shortener_api}`"""
 
@@ -149,18 +149,18 @@ This Text will be added to the **bottom** of every message **caption** or text
 For adding **line break** use \n
 To Remove Footer Text: `/footer remove`"""
 
-USERNAME_TEXT = """**Hello Harman, I am shortnerfly.com, Bulk Link Converter Bot From Linked shortnerfly.com Account,**
+USERNAME_TEXT = """**Hello Harman, I am kpslink.com, Bulk Link Converter Bot From Linked shortnerfly.com Account,**
 
 **🌟 Type** /channel (channel link or username)
 
 **example:**
-/channel @shortnerfly
+/channel @kpslink_official
 Or
-/channel https://t.me/shortnerfly
+/channel https://t.me/kpslink_official
 
 **🤘 Hit** 👉 /features To Know More Features Of This Bot.
 
-**- Message @cyniteofficial For More Help -**"""
+
 
 BANNER_IMAGE = """
 Usage: `/banner_image image_url` or reply to any Image with this command
